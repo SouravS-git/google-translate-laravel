4a3635305406b0cd505e223d378e05af94da228b
Sourav Sarkar <isourav2018@gmail.com>
2020-06-27 16:26:22 +0530

    added option to get back the values of submitted inputs after a successful form submission

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'TranslateController@index');
 
 Route::post('/translate', 'TranslateController@translate')->name('translate');
